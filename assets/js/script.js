039dd05b5cacc5725bede1f227e1fc6aa247e470
cgaetez <claudiagaetez@gmail.com>
2024-02-16 00:44:10 -0300

    recibieindo datos

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,15 @@ function valida(input){
         return numberRegex.test(input); 
 }
     
-
+function reset(){
+    $('#error').addClass('d-none');
+    $('#resultado').addClass('d-none');
+    $('#resultado  .connections').html("");
+}
 
     $('#buscar').on('click',function(e){
         e.preventDefault();
+        reset();
 
         if(valida($('#hero').val())){
             console.log('es número');
@@ -21,14 +26,31 @@ function valida(input){
                 dataType:"json",
                 success: 
                 function(datosApi) {
-                    console.log(datosApi.img);
-                      $('#resultado  img').attr("src",`${datosApi.image['url']}`);
-                      $('#resultado  .card-title').html(`Nombre: ${datosApi.name}`);
-                      
-                      $('#resultado').removeClass('d-none');
+                    if(datosApi.response != 'error'){
+                        console.log(datosApi);
+                        $('#resultado  img').attr("src",`${datosApi.image['url']}`);
+                        $('#resultado  .name').html(`Nombre: ${datosApi.name}`);
+                        /*
+                        datosApi.connections.forEach(element => {
+                        //$('#resultado  .connections').append(`${element}`);
+                        })*/
+  
+                        Object.entries(datosApi.connections).forEach(([key, value]) => {
+                          $('#resultado  .connections').append(`<p class="my-0">${key}: ${value}</p>`);
+                           
+                        });
+                        
+                        $('#resultado').removeClass('d-none');
+                    }else{
+                        console.log('no existe id');
+                        $('#error').removeClass('d-none');
+                    }                   
                 },
                 error: function(error) {
                     //si todo sale bien, se agrega la funcionalidad aquí.
+                    console.log('error acá');
+                    console.log(error);
+                    $('#error').removeClass('d-none');
                 },
             });
         }else{
